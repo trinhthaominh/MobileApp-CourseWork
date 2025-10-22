16ea340f979f197358d729c0b5a3b4dc5931dfb1
trinhthaominh <92960404+trinhthaominh@users.noreply.github.com>
2025-10-22 21:08:22 +0700

    Bài 5.1

--- TuHoc/Bai5_1XuLySuKien/app/src/main/java/com/example/bai5_1xulysukien/MainActivity.kt
+++ TuHoc/Bai5_1XuLySuKien/app/src/main/java/com/example/bai5_1xulysukien/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bai5_1xulysukien
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -24,7 +25,11 @@ class MainActivity : AppCompatActivity() {
         val edtNhapB = findViewById<EditText>(R.id.edtNhapB)
         val edtKq = findViewById<EditText>(R.id.edtKq)
         val btnCong = findViewById<Button>(R.id.btnCong)
+        val btnNhan = findViewById<Button>(R.id.btnNhan)
+        val btnChia = findViewById<Button>(R.id.btnChia)
         val btnReset = findViewById<Button>(R.id.btnReset)
+        val btnNhanLau = findViewById<Button>(R.id.btnNhanLau)
+        val btnDangXuat = findViewById<Button>(R.id.btnDangXuat)
 
 
 //        Set text cho edtNhapA
@@ -39,11 +44,46 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+
         btnReset.setOnClickListener {
             edtKq.setText("")
             edtNhapA.setText("")
             edtNhapB.setText("")
             Toast.makeText(this, "Em đã xóa hết rồi nhé", Toast.LENGTH_SHORT).show()
         }
+
+        var suKienChiaSe : View.OnClickListener? = null
+        suKienChiaSe = View.OnClickListener{
+            if (it == btnNhan) {
+                val A = edtNhapA.text.toString().toInt()
+                val B = edtNhapB.text.toString().toInt()
+                val kq = A * B
+                edtKq.setText(kq.toString())
+            } else if (it == btnChia) {
+                val A = edtNhapA.text.toString().toDouble()
+                val B = edtNhapB.text.toString().toDouble()
+                val kq = A / B
+                edtKq.setText(kq.toString())
+            }
+        }
+        btnNhan.setOnClickListener(suKienChiaSe)
+        btnChia.setOnClickListener(suKienChiaSe)
+
+        var suKienNhanLau : View.OnLongClickListener? = null
+        suKienNhanLau = View.OnLongClickListener{
+            if (it == btnNhanLau) {
+                Toast.makeText(this, "Long click detected", Toast.LENGTH_SHORT).show()
+                btnNhanLau.visibility = View.GONE // ẩn button
+            }
+            true
+        }
+
+        btnNhanLau.setOnLongClickListener(suKienNhanLau)
+
+        btnDangXuat.setOnClickListener {
+            Toast.makeText(this, "Gà lại lập trình" + "\nXin cảm ơn", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
     }
 }
